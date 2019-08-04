9c108b6070bcb9c1fa177309bd4862167dfdb030
Mohammad Soubra <mohammadsoubra@outlook.com>
2019-08-04 07:57:59 +0300

    Wire Up Login Form to API
    Connects login form button to authentication API endpoint (/Token) gets back the bearer token or an exception if faild.

---
+++ ASDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,55 @@
+﻿using ASDesktopUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASDesktopUI.Helpers
+{
+    class APIHelper : IAPIHelper
+    {
+        public HttpClient apiClient;
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+
+            apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri(api);
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applicatio/json"));
+        }
+
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            var date = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            using (HttpResponseMessage response = await apiClient.PostAsync("/Token", date))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
