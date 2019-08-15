4cb339d39893c63c70471f2b12d8d5635a7e9567
Mohammad Soubra <mohammadsoubra@outlook.com>
2019-08-15 08:32:45 +0300

    Update EFDatabase Name
    Update Database name to be EFData

--- ASDataManager/Models/IdentityModels.cs
+++ ASDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ASDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
