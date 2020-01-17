e08b73bb6aba11af2014468a2c47876ae37da6b0
Mohammad Soubra <mohammadsoubra@outlook.com>
2020-01-17 16:16:57 +0200

    Added WebUI

--- AccountingSoftwareDatabase/dbo/Tables/Invoice.sql
+++ AccountingSoftwareDatabase/dbo/Tables/Invoice.sql
@@ -5,5 +5,7 @@
     [PaymentDueDate] DATETIME2 NOT NULL, 
     [Description] VARCHAR(MAX) NULL, 
     [SaleDetailId] INT NOT NULL, 
-    CONSTRAINT [FK_Invoice_ToSaleDetail] FOREIGN KEY (SaleDetailId) REFERENCES SaleDetail(Id) 
+    [ClientId] INT NOT NULL, 
+    CONSTRAINT [FK_Invoice_ToSaleDetail] FOREIGN KEY (SaleDetailId) REFERENCES SaleDetail(Id) ,
+    CONSTRAINT [FK_Invoice_ToClient] FOREIGN KEY (ClientId) REFERENCES Client(Id) 
 )
