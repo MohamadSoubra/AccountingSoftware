105738427f31cae84989a30499d0b041427b23d4
Mohammad Soubra <mohammadsoubra@outlook.com>
2022-06-27 00:21:10 +0300

    Added Refresh Tokens

--- AccountingSoftwareApi/Data/Migrations/20201015053053_AddedRefreshTokens.Designer.cs
+++ AccountingSoftwareApi/Data/Migrations/20201015053053_AddedRefreshTokens.Designer.cs
@@ -4,14 +4,16 @@ using AccountingSoftwareApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AccountingSoftwareApi.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201015053053_AddedRefreshTokens")]
+    partial class AddedRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
