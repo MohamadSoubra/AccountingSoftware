562371c8edf3e39031359323f58fab0896c5502b
Mohammad Soubra <mohammadsoubra@outlook.com>
2022-06-27 00:23:36 +0300

    Changed token request logic

---
+++ AccountingSoftwareApi/Identity/AuthenticationResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountingSoftwareApi.Identity
+{
+    public class AuthenticationResult
+    {
+        public string Username { get; set; }
+        public string Access_Token { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public bool Success { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ AccountingSoftwareApi/Identity/IIdentityService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace AccountingSoftwareApi.Identity
+{
+    public interface IIdentityService
+    {
+        Task<AuthenticationResult> RefreshTokenAsync(string token, string refreshToken);
+        Task<AuthenticationResult> LoginAsync(string email, string password);
+    }
+}
