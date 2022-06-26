21573acba2555f69cbf374b0aa23f255469671b3
Mohammad Soubra <mohammadsoubra@outlook.com>
2022-06-27 00:11:12 +0300

    Added Controllers

---
+++ AccountingSoftwareApi/Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASDataManager.Library.DataAccess;
+using ASDataManager.Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingSoftwareApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : Controller
+    {
+        private readonly IClientData _clientData;
+
+        public ClientController(IClientData clientData)
+        {
+            _clientData = clientData;
+        }
+
+        [Authorize(Roles = "Accountant")]
+        [HttpGet]
+        public List<ClientModel> Get()
+        {
+            return _clientData.GetClients();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult PostClients([FromBody] List<ClientModel> clients)
+        {
+            _clientData.PostClients(clients);
+
+            return Ok();
+        }
+    }
+}
---
+++ AccountingSoftwareApi/Controllers/InvoiceController.cs
@@ -0,0 +1,49 @@
+﻿using ASDataManager.Library.DataAccess;
+using ASDataManager.Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AccountingSoftwareApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly IInvoiceData _invoiceData;
+
+        public InvoiceController(IInvoiceData invoiceData)
+        {
+            _invoiceData = invoiceData;
+        }
+
+        [Authorize(Roles = "Accountant")]
+        [HttpGet]
+        public List<InvoiceDBModel> GetInvoices()
+        {
+            return _invoiceData.GetAllInvoices();
+        }
+
+        [Authorize(Roles = "Accountant")]
+        [Route("GetInvoiceById")]
+        [HttpGet]
+        public InvoiceDBModel getInvoice(int InvoiceId)
+        {
+            return _invoiceData.GetInvoiceById(InvoiceId);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public void PostInvoice([FromBody] InvoiceModel invoice)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            _invoiceData.SaveInvoiceRecord(invoice, userId);
+        }
+    }
+}
