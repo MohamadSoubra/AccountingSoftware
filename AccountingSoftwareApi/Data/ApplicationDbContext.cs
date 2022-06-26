7593c9cd21fc92374e5c1fcc14dee63009ec84ae
Mohammad Soubra <mohammadsoubra@outlook.com>
2022-06-27 00:19:41 +0300

    Added Refresh Tokens to the database

--- AccountingSoftwareApi/Data/ApplicationDbContext.cs
+++ AccountingSoftwareApi/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AccountingSoftwareApi.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,12 @@ namespace AccountingSoftwareApi.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
