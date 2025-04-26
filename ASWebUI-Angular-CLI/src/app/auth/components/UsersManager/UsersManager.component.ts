59ab03e12af26d5a2edab5769cea5910932bc4b1
Mohammad Soubra <mohammadsoubra@outlook.com>
2025-04-26 13:39:35 +0300

    [Angular] change how users are retrieved in Users Manager component

--- ASWebUI-Angular-CLI/src/app/auth/components/UsersManager/UsersManager.component.ts
+++ ASWebUI-Angular-CLI/src/app/auth/components/UsersManager/UsersManager.component.ts
@@ -1,8 +1,8 @@
 import { Component,OnInit, ViewChild } from "@angular/core";
 import { AuthService } from "../../auth.service";
-import { apiUser } from "src/app/Models/apiUser.model";
 import { TableComponent } from "src/app/sharedFeatures/table/table.component";
 import { ActivatedRoute, Router } from "@angular/router";
+import { User } from "../../Models/User.model";
 
 @Component({
     selector: 'app-user-manager',
@@ -10,24 +10,33 @@ import { ActivatedRoute, Router } from "@angular/router";
     styleUrls: ['./UsersManager.component.scss']
 })
 
+
 export class UsersManagerComponent<T> implements OnInit {
     usersList: User[]
     usersTableColumns = [];
     componentName: string = "User";
     selectedUsers;
 
-    @ViewChild(TableComponent) table: TableComponent<apiUser>;
+    @ViewChild(TableComponent) table: TableComponent<User>;
     constructor(
         private auth: AuthService<T>,
         private router: Router,
         private activatedRoute: ActivatedRoute,
-    )
-        {}
+    ){}
 
 
     ngOnInit(){
 
-        this.auth.getUsers().subscribe(users => this.usersList = users as User[])
+        this.auth.getUsers().subscribe(users => this.usersList = users.map(user => {
+            return {
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                emailAddress: user.emailAddress,
+                roles: user.roles
+            } as User
+        }))
 
         this.usersTableColumns = [
             {
